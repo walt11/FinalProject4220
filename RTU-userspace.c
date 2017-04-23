2dab1658799ad7b05317875500794e67ce849e96
walt11 <johnwalt011@gmail.com>
2017-04-22 19:03:43 -0500

    added socket connections to allow real-time data to be sent from the RTU and for commands to be sent to the RTU

--- RTU-userspace.c
+++ RTU-userspace.c
@@ -1,48 +1,327 @@
 /*
-
-Author:		John Walter
-Project:	4220 Final Project
-
-*/
-
-
+ *  Created on: April 14, 2017
+ *      Author: walterjd
+ */
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/time.h>
 #include <pthread.h>
+#include <rtai.h>
+#include <rtai_lxrt.h>
 #include <fcntl.h>
 #include <semaphore.h>
+#include <sys/mman.h>
+#include <sys/types.h>
 
-sem_t sem;
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
 
-void read_fifo(){
-	int i,j;
-	int fifo_buffer[6][2];
-	int fd_fifo_in;
-	fd_fifo_in = open("/dev/rtf/0", O_RDWR); 
-	
+#define SERVER_ADDRESS "10.3.52.254"
+#define PORT_NUMBER 2221
+#define PORT_REC 2223
+
+RTIME BaseP;
+
+char buffer[100];
+int one;
+int two;
+pthread_t th1, th2, th3, th4, th5;
+int LED=0;
+struct Data {
+	// status
+	int b1;
+	int b2;
+	int b3;
+	int led1;
+	int led2;
+
+	// times
+	int b1_time[2];
+	int b2_time[2];
+	int b3_time[2];
+	int led1_time[2];
+	int led2_time[2];
+
+};
+struct Data d, prev, com;
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(0);
+}
+
+void Send_to_socket(void *x){
+	RT_TASK *rttask1 = rt_task_init(nam2num("th1"),0,512,256);
+	BaseP = start_rt_timer(nano2count(1000000000));
+	rt_task_make_periodic(rttask1,rt_get_time(),BaseP);
+
+	int sockfd;
+	int portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    portno = PORT_NUMBER;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); // Creates socket. Connection based.
+    if (sockfd < 0)
+        puts("Error opening socket");
+
+    server = gethostbyname(SERVER_ADDRESS);
+
+    // fill in fields of serv_addr
+    bzero((char *) &serv_addr, sizeof(serv_addr));	// sets all values to zero
+    serv_addr.sin_family = AF_INET;		// symbol constant for Internet domain
+
+    // copy to serv_addr.sin_addr.s_addr. Function memcpy could be used instead.
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+
+    serv_addr.sin_port = htons(portno);		// fill sin_port field
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0){
+	        puts("ERROR connecting to server. Writing to log instead");
+	        // NEED TO HANDLE THIS
+	}else{
+		puts("connection established");
+	}
+
+    while(1){
+    	printf("Thing\t\tStatus\tSeconds\t\tuSeconds\n");
+    	printf("-----\t\t------\t-------\t\t--------\n");
+    	printf("Button 1\t%d\t%d\t%d\n", d.b1, d.b1_time[0], d.b1_time[1]);
+    	printf("Button 2\t%d\t%d\t%d\n", d.b2, d.b2_time[0], d.b2_time[1]);
+    	printf("Button 3\t%d\t%d\t%d\n", d.b3, d.b3_time[0], d.b3_time[1]);
+    	printf("LED 1\t\t%d\t%d\t%d\n", d.led1, d.led1_time[0], d.led1_time[1]);
+    	printf("LED 2\t\t%d\t%d\t%d\n\n\n", d.led2, d.led2_time[0], d.led2_time[1]);
+
+    	// send message
+    	n = write(sockfd,&d,sizeof(d));	// sendto() could be used.
+    	if (n < 0){
+    		puts("Could not write to socket");
+    		// THIS IS PROBABLY WHERE WE NEED TO STORE THE UNSENT DATA
+    	}
+
+    	rt_task_wait_period();
+
+    }
+}
+
+
+
+void Button_FIFO(void *x){
+	int button=0;
+	int sec=0;
+	int usec=0;
+	int fd_fifo_in = open("/dev/rtf/0", O_RDWR);
 	while(1){
-		read(fd_fifo_in, &fifo_buffer, sizeof(fifo_buffer));
-		for(i=0;i<6;i++){
-			for(j=0;j<2;j++){
-				printf("%d ",fifo_buffer[i][j]);
-			}
-			printf("\n");
+		read(fd_fifo_in, &button, sizeof(int));
+		read(fd_fifo_in, &sec, sizeof(int));
+		read(fd_fifo_in, &usec, sizeof(int));
+		switch(button){
+			case 1:
+					if(d.b1 == 1){
+						d.b1 = 0;
+					}else{
+						d.b1 = 1;
+					}
+					d.b1_time[0] = sec;
+					d.b1_time[1] = usec;
+				break;
+			case 2:
+					if(d.b2 == 1){
+						d.b2 = 0;
+					}else{
+						d.b2 = 1;
+					}
+					d.b2_time[0] = sec;
+					d.b2_time[1] = usec;
+				break;
+			case 3:
+					if(d.b3 == 1){
+						d.b3 = 0;
+					}else{
+						d.b3 = 1;
+					}
+					d.b3_time[0] = sec;
+					d.b3_time[1] = usec;
+				break;
 		}
-		printf("\n\n");
+	}
+}
+void LED_FIFO(void *x){
+	struct timeval t;
+	int fd_fifo_in = open("/dev/rtf/2", O_RDWR);
+	while(1){
+		if(read(fd_fifo_in, &t, sizeof(t)) == -1){
+			puts("Read from fifo 2 error");
+		}
+		if(LED == 1){
+			// put time retrieved into time for LED 1
+			d.led1_time[0] = t.tv_sec;
+			d.led1_time[1] = t.tv_usec;
+		}else{
+			// put time retrieved into time for LED 2
+			d.led2_time[0] = t.tv_sec;
+			d.led2_time[1] = t.tv_usec;
+		}
 	}
 }
 
-int main(void)
-{
-	int ready_fifo;
-	pthread_t readFifo;
-	ready_fifo = open("/dev/rtf/1", O_RDWR);
-	// writes a 1 to the fifo to tell the module that it is ready to receive data
-	write(ready_fifo, 1, sizeof(int)); 
-	
-	//created thread to test reading from the fifo, kernel module must be installed on board first
-	pthread_create(&readFifo, NULL, (void*)&read_fifo,NULL);
-	while(1){}
+void LED_Test(void *x){
+	unsigned long *PBDR, *PBDDR, *ptr;
+	int fd_fifo_in = open("/dev/rtf/1", O_RDWR);
+	int fd;
+		// open the file /dev/mem
+		fd = open("/dev/mem", O_RDWR|O_SYNC); // O_RDWR|O_SYNC
+
+		// if the file could not be opened - error out
+
+
+		// point to the memory location of the board
+		ptr = (unsigned long *)mmap(NULL,4096,PROT_READ|PROT_WRITE,MAP_SHARED, fd, 0x80840000); // 4096
+		// if the memory map could not be completed
+
+		// Access B port
+		PBDR = ptr + 1; 	// 0x80840004
+		PBDDR = ptr + 5; 	// 0x80840014
+		*PBDDR &= 0xFFFFFFF7;
+		*PBDDR &= 0xFFFFFFEF;
+		int bit, bit2;
+		int write_v=0;
+		int value1 = 0;
+		int value2 = 0;
+		// NOT writing to FIFO???
+		while(1){
+			bit = ((*PBDR >> 3) & 0x01);
+			if(bit == 0){
+				LED = 1;
+				printf("Button press detected\n");
+				if(value1 == 1){ // led1 is on
+					value1 = 0;
+					d.led1 = 0;
+					write_v = 2;
+				}else{
+					d.led1 = 1;
+					value1 = 1;
+					write_v = 1;
+				}
+				write(fd_fifo_in,&write_v,sizeof(int));
+			}
+
+			bit2 = ((*PBDR >> 4) & 0x01);
+			if(bit2 == 0){
+				LED = 2;
+				printf("Button press detected\n");
+				if(value2 == 1){ // led1 is on
+					value2 = 0;
+					d.led2 = 0;
+					write_v = 4;
+				}else{
+					d.led2 = 1;
+					value2 = 1;
+					write_v = 3;
+				}
+				write(fd_fifo_in,&write_v,sizeof(int));
+			}
+
+			usleep(100000);
+		}
+
+}
+
+void Rec_Command(void *x){
+	int sock, length, n;
+	int command=0;
+	   int boolval = 1;		// for a socket option
+	   socklen_t fromlen;
+	   struct sockaddr_in server;
+	   struct sockaddr_in from;
+
+
+	   sock = socket(AF_INET, SOCK_DGRAM, 0); // Creates socket. Connectionless.
+	   if (sock < 0)
+		   error("Opening socket");
+
+	   length = sizeof(server);			// length of structure
+	   bzero(&server,length);			// sets all values to zero. memset() could be used
+	   server.sin_family = AF_INET;		// symbol constant for Internet domain
+	   server.sin_addr.s_addr = INADDR_ANY;		// IP address of the machine on which
+												// the server is running
+	   server.sin_port = htons(PORT_REC);	// port number
+
+	   // binds the socket to the address of the host and the port number
+	   if (bind(sock, (struct sockaddr *)&server, length) < 0)
+	       error("binding");
+
+	   // set broadcast option
+	   if (setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &boolval, sizeof(boolval)) < 0)
+	   	{
+	   		printf("error setting socket options\n");
+	   		exit(-1);
+	   	}
+
+	   fromlen = sizeof(struct sockaddr_in);	// size of structure
+
+	   while (1)
+	   {
+		   // receive from client
+	       n = recvfrom(sock, &command, sizeof(int), 0, (struct sockaddr *)&from, &fromlen);
+	       if (n < 0)
+	    	   error("recvfrom");
+	       printf("Command: %d\n",command);
+	   }
+}
+
+
+int main(void){
+	BaseP = start_rt_timer(nano2count(1000000));
+	pthread_create(&th1, NULL, (void*)Send_to_socket, NULL);
+	pthread_create(&th2, NULL, (void*)Button_FIFO, NULL);
+	pthread_create(&th3, NULL, (void*)LED_FIFO, NULL);
+	pthread_create(&th4, NULL, (void*)LED_Test, NULL);
+	pthread_create(&th5, NULL, (void*)Rec_Command, NULL);
+
+	pthread_join(th1,0);
+	pthread_join(th2,0);
+	pthread_join(th3,0);
+	pthread_join(th4,0);
+	pthread_join(th5,0);
+	//int choice=0;
+	//int fd_fifo_in = open("/dev/rtf/1", O_RDWR);
+	while(1){
+		/*
+		while((choice<1) | (choice > 4)){
+			printf("Pick:\n\t(1) LED 1 ON\n\t(2) LED 1 OFF\n\t(3) LED 2 ON\n\t(4) LED 2 OFF\n> ");
+			scanf("%d",&choice);
+		}
+		write(fd_fifo_in,&choice,sizeof(int));
+		switch(choice){
+			case 1:
+				d.led1 = 1;
+				LED = 1;
+				break;
+			case 2:
+				d.led1 = 0;
+				LED = 1;
+				break;
+			case 3:
+				d.led2 = 1;
+				LED = 2;
+				break;
+			case 4:
+				d.led2 = 0;
+				LED = 2;
+				break;
+		}
+
+		// will have to write to FIFO 1 for LEDs
+		// 1 = turn on led 1	global LED = 1
+		// 2 = turn off led 1	global LED = 1
+		// 3 = turn on led 2	global LED = 2
+		// 4 = turn off led 2	global LED = 2
+	*/
+	}
+
+	return 0;
 }
--- RTU_kernel.c
+++ RTU_kernel.c
@@ -24,141 +24,74 @@ Project:	4220 Final Project
 #include <linux/time.h>
 #include <rtai_fifos.h>
 
+
 MODULE_LICENSE("GPL");
-unsigned long *ptr, *PBDR, *PBDDR, *PFDR, *PFDDR, *GPIOBIntType1, *GPIOBIntType2, *GPIOBEOI, *GPIOBIntEn, *IntStsB, *RawIntStsB;
+unsigned long *ptr, *PBDR, *PBDDR, *PFDR, *PFDDR, *GPIOBIntType1, *GPIOBIntType2, *GPIOBEOI, *GPIOBIntEn, *IntStsB, *RawIntStsB, *Debounce;
 SEM sem;
-static RT_TASK mytask, LED_Task;
+static RT_TASK LED_Task, Analog_Task;
 RTIME period;
-int data[6][2];
-int b1=0, b2=0, b3=0;
-static void MainTask(int t){
-	//int shift=0;
-	//int bit=0;
-	int value=0;
-	unsigned long *ptr, *PBDR, *PBDDR;
-	ptr = (unsigned long *)__ioremap(0x80840000,4096,0);
-	PBDR = ptr + 1;
-	PBDDR = ptr + 5;
-	*PBDDR &= 0xFFFFFFFE;
-	while(1){
-		//int data[6][2];
-		/*for(i=0;i<6;i++){
-			for(j=0;j<2;j++){
-				data[i][j] = value;
-			}
-		}*/
-		value++;
-		rt_sem_wait(&sem);
-			data[5][0] = value;
-			rtf_put(0,&data,sizeof(data));
-		rt_sem_signal(&sem);
-		rt_sleep(nano2count(1000000000));
-	}
-
-	//while(1){
-		//struct timeval t;
-			// continuously check the bit corresponding to the button to see if it changes
-			//bit = ((*PBDR >> shift) & 0x01);
-			// if the bit goes from 1 to 0 (the button was pressed)
-			//if(bit == 0){
-				//do_gettimeofday(&t);
-				//int sec = t.tv_sec;
-				//int usec = t.tv_usec;
-				//rtf_put(0, &sec ,sizeof(sec));
-				//rtf_put(0,&usec,sizeof(usec));
-
-		//}
-}
-
 
 static void my_handler(unsigned irq_num, void *cookie){
+
 	rt_disable_irq(59);
 	struct timeval t;
 	do_gettimeofday(&t);
 	int sec = t.tv_sec;
-
+	int usec = t.tv_usec;
 	int i, bit, button;
 	i=0;
 	bit=0;
 	button=0;
 	for(i=0;i<3;i++){
-		bit = ((*RawIntStsB >> i) & 0x01);
-		if(bit != 0){
-			button = i;
+		if((*RawIntStsB & 0x01) == 0x01){
+			button = 1;
+		}else if((*RawIntStsB & 0x02) == 0x02){
+			button = 2;
+		}else{
+			button = 3;
 		}
+		printk("Button pushed: %d\n",button);
 	}
+	rtf_put(0,&button,sizeof(int));
+	rtf_put(0,&sec,sizeof(int));
+	rtf_put(0,&usec,sizeof(int));
 	//printk("Button %d was pressed\n",button);
-	rt_sem_wait(&sem);
 	if((button == 0)){
-			if(data[0][0] == 1){ // if "switch" is on
-				data[0][0] = 0;	 // button press indicates switch flips to off
-			}else{
-				data[0][0] = 1;
-			}
-			data[0][1] = sec;
-
 		*GPIOBEOI |= 0x01;
-
 	}else if((button == 1)){
-			if(data[1][0] == 1){ // if "switch" is on
-				data[1][0] = 0;	 // button press indicates switch flips to off
-			}else{
-				data[1][0] = 1;
-			}
-			data[1][1] = sec;
-
 		*GPIOBEOI |= 0x02;
-
-	}else if((button == 2)){
-			if(data[2][0] == 1){ // if "switch" is on
-				data[2][0] = 0;	 // button press indicates switch flips to off
-			}else{
-				data[2][0] = 1;
-			}
-			data[2][1] = sec;
-
+	}else{
 		*GPIOBEOI |= 0x04;
-	}else{}
-	rt_sem_signal(&sem);
+	}
 	rt_enable_irq(59);
 }
 
 static void LEDTask(int x){
+	//******* It is not reading from FIFO 1 or writing to fifo 2
+	printk("LEDTask started");
+	int buf = 0;
 	struct timeval t;
-	int sec =0;
-	int bit=0;
+	int sec = 0;
+	int usec = 0;
+
 	while(1){
-		// get LED 1 current status
-		//bit = ((*PBDR >> 5) & 0x01);
-
-
-		bit = ((*PBDR >> 5)&0x01);
-		if(bit != data[3][0]){ // if the bit has changed
+		if(rtf_get(1, &buf, sizeof(buf)) > 0){
+			printk("FROM FIFO 1: %d\n",buf);
 			do_gettimeofday(&t);
-			sec = t.tv_sec;
-			rt_sem_wait(&sem);
-			data[3][0] = bit;
-			data[3][1] = sec;
-			rt_sem_signal(&sem);
+			printk("sec: %d \tusec: %d\n", t.tv_sec, t.tv_usec);
+			rtf_put(2, &t, sizeof(t));
+
 		}
-
-
-		// get LED 2 current status
-		bit = ((*PBDR >> 6) & 0x01);
-		if(bit != data[4][0]){ // if the bit has changed
-			do_gettimeofday(&t);
-			sec = t.tv_sec;
-			rt_sem_wait(&sem);
-			data[4][0] = bit;
-			data[4][1] = sec;
-			rt_sem_signal(&sem);
-		}
-
 		rt_sleep(nano2count(50000000));
 	}
 }
+/*
+static void AnalogTask(int t){
 
+}
+*/
 int init_module(void){
+	printk("Module started\n");
 	ptr = (unsigned long *)__ioremap(0x80840000,4096,0);
 	PBDR = ptr+1;
 	PBDDR = ptr+5;
@@ -168,27 +101,25 @@ int init_module(void){
 	GPIOBIntEn 		= (unsigned long *)__ioremap(0x808400B8, 4096,0);
 	IntStsB 		= (unsigned long *)__ioremap(0x808400BC, 4096,0);
 	RawIntStsB	 	= (unsigned long *)__ioremap(0x808400C0, 4096,0);
+	Debounce		= (unsigned long *)__ioremap(0x808400C4, 4096,0);
 
 	*PBDDR &= 0xF8;			//1111 1000
-	*PBDDR &= 0x60; // set leds as output
+	*PBDDR &= 0x60; 		//set leds as output
 	*GPIOBIntEn |= 0x07;	//0000 0111
 	*GPIOBIntType1 |= 0x07;	//0000 0111
 	*GPIOBIntType2 &= 0xF8; //1111 1000
+	*Debounce |= 0x07;		//0000 0111
 
-	int dummy[6][2];
-	int buf=0;
+	struct timeval t;
 	rt_set_periodic_mode();
 	period = start_rt_timer(nano2count(1000000));
 	rt_sem_init(&sem,1);
-	rtf_create(0,sizeof(dummy));
+	rtf_create(0,sizeof(int));
+	printk("Created fifo 0\n");
 	rtf_create(1,sizeof(int));
-	
-	// this waits for the userspace program to become ready so that the data being
-	// collected does not build up in fifo 0, when a 1 is read from the fifo, then
-	// the tasks are initialzied and started
-	while(buf!=1){
-		rtf_get(1,&buf,sizeof(int));
-	}
+	printk("Created fifo 1\n");
+	rtf_create(2,sizeof(t));
+	printk("Created fifo 2\n");
 
 	rt_request_irq(59, my_handler, 0, 1);
 	rt_enable_irq(59);
@@ -197,17 +128,21 @@ int init_module(void){
 	// initialize a semaphore
 
 	// initialize real time task and make it periodic
-	rt_task_init(&mytask, MainTask,0,256,0,0,0);
 	rt_task_init(&LED_Task, LEDTask, 0, 256,0,0,0);
-	rt_task_resume(&mytask);
+	//rt_task_init(&Analog_Task, AnalogTask, 0, 256, 0, 0, 0);
+	//rt_task_resume(&Analog_Task);
 	rt_task_resume(&LED_Task);
 	return 0;
 }
 
 void cleanup_module(void){
-	rt_task_delete(&mytask);
 	rt_task_delete(&LED_Task);
+	rt_task_delete(&Analog_Task);
+
 	stop_rt_timer();
+	rtf_destroy(0);
+	rtf_destroy(1);
+	rtf_destroy(2);
 	rt_disable_irq(59);
 	rt_sem_delete(&sem);
 }
